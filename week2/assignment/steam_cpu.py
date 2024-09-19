c4a4b029629db3c381b405c3c4e5e86dfec3fed1
angusseung <angus_seung@outlook.com>
2024-09-19 16:09:26 +0800

    Scrape Titles

--- week2/assignment/steam_cpu.py
+++ week2/assignment/steam_cpu.py
@@ -6,10 +6,18 @@ import re
 page_to_scrape = requests.get("https://store.steampowered.com/hwsurvey/cpus/")
 soup = BeautifulSoup(page_to_scrape.text, "html.parser")
 
+titles = soup.findAll("div", attrs={"class": "substats_col_left"})
 stats = soup.findAll("div", attrs={"class": "substats_col_month"})
 
+titleTexts = []
+finalTitles = []
 statValues = []
 statTotals = []
+
+for title in titles:
+    titleTexts.append(title.text)
+
+finalTitles = [text for text in titleTexts if "PHYSICAL" not in text]
 
 for stat in stats:
     stat_text = stat.get_text().strip()
