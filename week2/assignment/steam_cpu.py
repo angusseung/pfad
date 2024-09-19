0f755bba9f65bd57caba1ec56026869fbd5a9956
angusseung <angus_seung@outlook.com>
2024-09-19 15:18:22 +0800

    Sum up total usage of each cpu

--- week2/assignment/steam_cpu.py
+++ week2/assignment/steam_cpu.py
@@ -6,6 +6,7 @@ page_to_scrape = requests.get("https://store.steampowered.com/hwsurvey/cpus/")
 soup = BeautifulSoup(page_to_scrape.text, "html.parser")
 stats = soup.findAll("div", attrs={"class": "substats_col_month"})
 statValues = []
+statTotals = []
 
 for stat in stats:
     stat_text = stat.get_text().strip()
@@ -17,3 +18,9 @@ for stat in stats:
 
 for statValue in statValues:
     print(statValue)
+
+for i in range(0, len(statValues), 4):
+    total = sum(statValues[i:i+4])
+    statTotals.append(total)
+    
+print(statTotals)
